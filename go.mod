2c5d8ef45a8a88ac67986a5364feaa189a2a5436
Yerden <erden.mangibayev@gmail.com>
2023-12-04 06:30:14 +0600

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/YnMann/social-media_backend
 
 go 1.21.4
 
-require github.com/BurntSushi/toml v1.3.2 // indirect
+require (
+	github.com/BurntSushi/toml v1.3.2
+	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/sys v0.15.0 // indirect
+)
--- internal/app/api-server/api-server/config.go
+++ internal/app/api-server/api-server/config.go
@@ -4,11 +4,13 @@ package api_server
 type Config struct {
 	// Адрес по которому мы запускаем веб сервер
 	BindAddr string `toml:"bind_addr"`
+	LogLevel string `toml:"log_level`
 }
 
 // Передает инициализированный config, default параметрами
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
+		LogLevel: "debug",
 	}
 }
