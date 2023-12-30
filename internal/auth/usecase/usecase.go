cf16765ab6f5321e8a41aa5f9a9a45cda303ceb3
Yerden <erden.mangibayev@gmail.com>
2023-12-31 03:15:56 +0600

    update

--- internal/auth/usecase/usecase.go
+++ internal/auth/usecase/usecase.go
@@ -23,6 +23,20 @@ type AuthUseCase struct {
 	expireDuration time.Duration
 }
 
+func NewAuthUseCase(
+	userRepo auth.UserRepository,
+	hashSalt string,
+	signingKey []byte,
+	tokenTTLSeconds time.Duration,
+) *AuthUseCase {
+	return &AuthUseCase{
+		userRepo:       userRepo,
+		hashSalt:       hashSalt,
+		signingKey:     signingKey,
+		expireDuration: time.Second * tokenTTLSeconds,
+	}
+}
+
 func (a *AuthUseCase) SignUp(ctx context.Context, username string, password string) error {
 	pwd := sha1.New()
 	pwd.Write([]byte(password))
@@ -36,7 +50,7 @@ func (a *AuthUseCase) SignUp(ctx context.Context, username string, password stri
 	return a.userRepo.CreateUser(ctx, user)
 }
 
-func (a *AuthUseCase) SignIn(ctx context.Context, username string, password string) error {
+func (a *AuthUseCase) SignIn(ctx context.Context, username string, password string) (string, error) {
 	pwd := sha1.New()
 	pwd.Write([]byte(password))
 	pwd.Write([]byte(a.hashSalt))
@@ -44,17 +58,39 @@ func (a *AuthUseCase) SignIn(ctx context.Context, username string, password stri
 
 	user, err := a.userRepo.GetUser(ctx, username, password)
 	if err != nil {
-		return "", auth.ErrUserNotFound
+		return "", auth.ErrInvalidAccessToken
 	}
 
 	claims := AuthClaims{
 		User: user,
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: jwt.At(time.Now().Add(a.expireDuration)),
+			ExpiresAt: time.Now().Add(a.expireDuration).Unix(),
 		},
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return token.SignedString(a.signingKey)
+}
+
+func (a *AuthUseCase) ParseToken(ctx context.Context, accessToken string) (*models.User, err) {
+	token, err := jwt.ParseWithClaims(
+		accessToken,
+		&AuthClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+			return a.signingKey, nil
+		})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*AuthClaims); ok && token.Valid {
+		return claims.User, nil
+	}
+
+	return nil, auth.ErrInvalidAccessToken
 }
--- internal/server/app.go
+++ internal/server/app.go
@@ -3,6 +3,9 @@ package server
 import (
 	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -12,31 +15,38 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
-	// auth
-	a_http 
+	"github.com/YnMann/chat_backend/internal/auth"
+
+	// a = auth
+	ahttp "github.com/YnMann/chat_backend/internal/auth/delivery/http"
+	amongo "github.com/YnMann/chat_backend/internal/auth/repository/mongo"
+	ausecase "github.com/YnMann/chat_backend/internal/auth/usecase"
 )
 
 // APIServer
 type App struct {
 	logger *logrus.Logger
 	router *mux.Router
+
+	httpServer *http.Server
+	authUC     auth.UseCase
 	// store  *store.Store
 }
 
 // New
 func NewApp() *App {
-	// db := initDb()
+	db := initDb()
 
-	// userRepo := authmongo.NewUserRepository(db, viper.GetString("mongo.user_collection"))
+	userRepo := amongo.NewUserRepository(db, viper.GetString("mongo.user_collection"))
 
 	return &App{
 		logger: logrus.New(),
-		// authUC: authusecase.NewAuthUseCase(
-		// 	userRepo,
-		// 	viper.GetString("auth.hash_salt"),
-		// 	[]byte(viper.GetString("auth.signing_key")),
-		// 	viper.GetDuration("auth.token_ttl"),
-		// ),
+		authUC: ausecase.NewAuthUseCase(
+			userRepo,
+			viper.GetString("auth.hash_salt"),
+			[]byte(viper.GetString("auth.signing_key")),
+			viper.GetDuration("auth.token_ttl"),
+		),
 	}
 }
 
@@ -47,39 +57,39 @@ func (a *App) Run(port string) error {
 		gin.Recovery(),
 		gin.Logger(),
 	)
-	
+
 	// Set up http handlers
 	// SignUp/SignIn endpoints
 	ahttp.RegisterHTTPEndpoints(router, a.authUC)
 
 	// API endpoints
-	authMiddleware := ahttp.NewAuthMiddleware(a.authUC)
-	api := router.Group("/api", authMiddleware)
+	// authMiddleware := ahttp.NewAuthMiddleware(a.authUC)
+	// api := router.Group("/api", authMiddleware)
 
 	// HTTP server
 	a.httpServer = &http.Server{
-		Addr: ":" + port,
-		Handler: router,
-		ReadTimeout: 10 * time.Second,
-		WriteTimeout: 10 * time.Second,
+		Addr:           ":" + port,
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	go func () {
+	go func() {
 		if err := a.httpServer.ListenAndServe(); err != nil {
 			log.Fatalf("Failed to listen and server: %+v", err)
 		}
-	}
-	
-	quit := make(chan os.Signal, 1) 
+	}()
+
+	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, os.Interrupt)
 
-	<- quit 
+	<-quit
 
 	ctx, shutdown := context.WithTimeout(context.Background(), 5*time.Second)
 	defer shutdown()
 
-	return a.httpServer.shutdown(ctx)
+	return a.httpServer.Shutdown(ctx)
 }
 
 func initDb() *mongo.Database {
